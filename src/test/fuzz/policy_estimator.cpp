1e13691690b94ffa3e5d668518b235f2f9d55a3e
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2024-06-13 19:09:49 +0100

    bug catch

--- src/test/fuzz/policy_estimator.cpp
+++ src/test/fuzz/policy_estimator.cpp
@@ -73,6 +73,7 @@ FUZZ_TARGET(policy_estimator, .init = initialize_policy_estimator)
                 std::vector<RemovedMempoolTransactionInfo> txs;
                 txs.reserve(mempool_entries.size());
                 for (const CTxMemPoolEntry& mempool_entry : mempool_entries) {
+                    CFeeRate(mempool_entry.GetFee(), mempool_entry.GetTxSize());
                     txs.emplace_back(mempool_entry);
                 }
                 block_policy_estimator.processBlock(txs, fuzzed_data_provider.ConsumeIntegral<unsigned int>());
