bebb8fabbc70074ba0bc348a8194ac8f23b1d2c9
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-01-09 10:50:53 -0500

    init: fail to start when `-blockmaxweight` exceeds `MAX_BLOCK_WEIGHT`

--- src/init.cpp
+++ src/init.cpp
@@ -1015,6 +1015,13 @@ bool AppInitParameterInteraction(const ArgsManager& args)
         }
     }
 
+    if (args.IsArgSet("-blockmaxweight")) {
+        const auto max_block_weight = args.GetIntArg("-blockmaxweight", DEFAULT_BLOCK_MAX_WEIGHT);
+        if (max_block_weight > DEFAULT_BLOCK_MAX_WEIGHT) {
+            return InitError(strprintf(_("Specified -blockmaxweight (%d) exceeds consensus maximum block weight (%d)"), max_block_weight, DEFAULT_BLOCK_MAX_WEIGHT));
+        }
+    }
+
     nBytesPerSigOp = args.GetIntArg("-bytespersigop", nBytesPerSigOp);
 
     if (!g_wallet_init_interface.ParameterInteraction()) return false;
