72144fdafeaa7576cc7aedeb4d59d84c29ff0238
ismaelsadeeq <abubakarsadiqismail@proton.me>
2026-01-13 15:04:05 +0000

    miner: clear block template after block connection/disconnection
    - This commit makes the blocktemplatecache subscribe to validation
      interface for all block connection and block disconnection
      notifications.
      Whenever such notification is emitted the block template cache is
      cleared.
    
    - For block connection notification we onlu clear for non historical
      block connection to avoid clearing during assumeutxo background
      validation.

--- src/node/miner.h
+++ src/node/miner.h
@@ -7,14 +7,15 @@
 #define BITCOIN_NODE_MINER_H
 
 #include <interfaces/types.h>
+#include <kernel/types.h>
 #include <node/types.h>
 #include <policy/policy.h>
 #include <primitives/block.h>
-#include <sync.h>
 #include <txmempool.h>
 #include <util/feefrac.h>
 #include <util/time.h>
 #include <validation.h>
+#include <validationinterface.h>
 
 #include <boost/multi_index/identity.hpp>
 #include <boost/multi_index/indexed_by.hpp>
@@ -27,6 +28,7 @@
 #include <deque>
 #include <memory>
 #include <optional>
+#include <utility>
 
 class ArgsManager;
 class CBlockIndex;
@@ -36,6 +38,7 @@ class CScript;
 namespace Consensus { struct Params; };
 
 using interfaces::BlockRef;
+using kernel::ChainstateRole;
 
 namespace node {
 class KernelNotifications;
@@ -160,8 +163,12 @@ bool ShareableOptions(const BlockAssembler::Options& a, const BlockAssembler::Op
  *   stored in the cache, and returned.
  * - If the interval is 0 we always create a new template and insert into the cache.
  * - After an insertion to the cache, we evict the oldest template if the cache overflows.
+ *
+ * The cache inherits from CValidationInterface to receive notifications
+ * about connected and disconnected blocks, which triggers cache invalidation,
+ * ensuring stale templates are not returned.
  */
-class BlockTemplateCache
+class BlockTemplateCache : public CValidationInterface
 {
 private:
     std::deque<std::pair<BlockAssembler::Options, BlockTemplateRef>> m_block_templates;
@@ -174,7 +181,7 @@ private:
 
 public:
     BlockTemplateCache(CTxMemPool& mempool, ChainstateManager& chainman, size_t block_template_block_template_cache_size = DEFAULT_BLOCK_TEMPLATE_CACHE_SIZE);
-    ~BlockTemplateCache() = default;
+    virtual ~BlockTemplateCache() = default;
 
     /**
      * If a cached template exists with identical options and its age is less than
@@ -185,6 +192,12 @@ public:
      * @return a BlockTemplateRef.
      */
     BlockTemplateRef GetBlockTemplate(const BlockAssembler::Options& options) EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
+
+    /** Overridden from CValidationInterface. */
+    void BlockConnected(const ChainstateRole& role, const std::shared_ptr<const CBlock>& /*unused*/, const CBlockIndex* /*unused*/)
+        override EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
+    void BlockDisconnected(const std::shared_ptr<const CBlock>& /*unused*/, const CBlockIndex* /*unused*/)
+        override EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
 };
 
 /**
